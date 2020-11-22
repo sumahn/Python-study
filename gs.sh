1caf2cf2ce0616efe88b2325a9e61346256c57b6
suman <cktnaks1@gmail.com>
2020-11-22 18:26:18 +0900

    first a.py

--- gs.sh
+++ gs.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-git pull 
+DATE=`date +%Y-%m-%d`
+MSG="$DATE lesson"
+if [ $# -gt 0 ]; then
+    MSG="$1"
+fi
+
+#git checkout master
+
+git diff --numstat origin/master
+
 git add --all
-git commit -m "lesson source"
-
-
+git commit -am "${MSG}"
 
 git push origin master
-
 
 # ./gs.sh 를 시행하면 git에 바로 넣을 수 있음.
