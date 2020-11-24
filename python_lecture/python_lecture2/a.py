07dfca39a51d886da3edb6b59930b1d5a51f79c5
suman <cktnaks1@gmail.com>
2020-11-24 16:47:37 +0900

    2020-11-24 lesson

--- python_lecture/python_lecture2/a.py
+++ python_lecture/python_lecture2/a.py
@@ -1,3 +1,41 @@
-print("Hello World!!")
+class TestClass:
+    name = "TEST"
 
-print("This is gitlens test")
+    @staticmethod
+    def static_method():
+        print("STATIC!!")
+
+    def get_name(self):
+        print("QQQQQQQQQQQ")
+        return self.name
+
+    def area(self, x, y):
+        return x * y
+
+class Child(TestClass):
+    
+    def get_name(self):
+        super().get_name()
+        return "Child Name: " + self.name
+
+    def area(self, x, y):
+        t = super().area(x, y)
+        return t / 2
+
+
+test = TestClass()
+child = Child()
+
+# hash, id 
+print(hash(test), id(test))
+# getattr
+a = getattr(test, 'get_name') # self가 붙어 있기 때문에 instance를 줘야 함.
+b = getattr(TestClass, 'static_method') # static method는 class 그대로 가져올 수 있음.
+a()
+b()
+
+print("1111>> ", child.get_name())
+
+c = callable(test.get_name)
+print("cccccccc>> ", c)
+
