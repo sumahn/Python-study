d8c8e920ec1a97a71ca4f59fe60c1e1ceb9831d3
suman <cktnaks1@gmail.com>
2021-01-12 20:48:16 +0900

    2021-01-12

--- PYTHONWORKSPACE/practice.py
+++ PYTHONWORKSPACE/practice.py
@@ -309,17 +309,82 @@
 
 # 예외 처리 
 
-try:
-    print("나누기 전용 계산기입니다.")
-    nums = []
-    nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
-    nums.append(int(input("두 번째 숫자를 입력하세요 : ")))
-    # nums.append(int(nums[0]/nums[1]))
-    print("{} / {} = {}".format(nums[0], nums[1], nums[2]))
-except ValueError:
-    print("에러! 잘못된 값을 입력하였습니다.")
-except ZeroDivisionError as err:
-    print(err)
-except Exception as err:
-    print("알 수 없는 에러가 발생하였습니다.")  
-    print(err)
+# try:
+#     print("나누기 전용 계산기입니다.")
+#     nums = []
+#     nums.append(int(input("첫 번째 숫자를 입력하세요 : ")))
+#     nums.append(int(input("두 번째 숫자를 입력하세요 : ")))
+#     # nums.append(int(nums[0]/nums[1]))
+#     print("{} / {} = {}".format(nums[0], nums[1], nums[2]))
+# except ValueError:
+#     print("에러! 잘못된 값을 입력하였습니다.")
+# except ZeroDivisionError as err:
+#     print(err)
+# except Exception as err:
+#     print("알 수 없는 에러가 발생하였습니다.")  
+#     print(err)
+
+
+# 의도적 에러 발생
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     num2 = int(input("두 번째 숫자를 입력하세요 : "))
+#     if num1 >= 10 or num2 >= 10:
+#         raise ValueError
+#     print("{} / {} = {}".format(num1, num2, int(num1/num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+
+
+# 사용자 정의 에러 발생 
+# class BigNumberError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+
+#     def __str__(self):
+#         return self.msg
+
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     num2 = int(input("두 번째 숫자를 입력하세요 : "))
+#     if num1 >= 10 or num2 >= 10:
+#         raise BigNumberError("입력값 : {}, {}".format(num1, num2))
+#     print("{} / {} = {}".format(num1, num2, int(num1/num2)))
+# except BigNumberError as err:
+#     print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")
+#     print(err)
+# finally: # 에러가 발생하든 하지 않든 시행
+#     print("계산기를 이용해주셔서 감사합니다.")
+
+
+
+
+# 모듈 
+# import theater_module as mv
+# mv.price(3) 
+# mv.price_morning(4)
+# mv.price_soldier(5)
+
+# from theater_module import *
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+
+
+
+# 패키지
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+from travel import *
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+
+import inspect
+import random
+print(inspect.getfile(random))
+print(inspect.getfile(thailand))
